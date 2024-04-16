01f354ddd75ac18bd4ec750b5ac8b39062d93823
Brian <briannbig@gmail.com>
2024-04-16 16:53:21 +0300

    define domain events

---
+++ internal/domain/event/events.go
@@ -0,0 +1,49 @@
+package event
+
+import "github.com/briannbig/afya-village/internal/domain/model"
+
+type (
+	PatientRegisteredEvent struct {
+		Patient model.Patient
+	}
+
+	PatientUpdatedEvent struct {
+		Patient model.Patient
+	}
+
+	MedicalProfessionalRegisteredEvent struct {
+		MedicalProfessional model.Medic
+	}
+
+	MedicalProfessionalUpdatedEvent struct {
+		MedicalProfessional model.Medic
+	}
+
+	MobileClinicCreatedEvent struct {
+		MobileClinic model.Clinic
+	}
+
+	MobileClinicUpdatedEvent struct {
+		MobileClinic model.Clinic
+	}
+
+	AppointmentScheduledEvent struct {
+		Appointment model.Appointment
+	}
+
+	AppointmentCompletedEvent struct {
+		Appointment model.Appointment
+	}
+
+	AppointmentCanceledEvent struct {
+		Appointment model.Appointment
+	}
+
+	MedicalRecordCreatedEvent struct {
+		MedicalRecord model.MedicalRecord
+	}
+
+	MedicalRecordUpdatedEvent struct {
+		MedicalRecord model.MedicalRecord
+	}
+)
