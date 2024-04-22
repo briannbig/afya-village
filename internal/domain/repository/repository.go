0e54235de29a6ba717c2e010d622fecde8642c13
Brian <briannbig@gmail.com>
2024-04-22 13:00:56 +0300

    create schedule appointment  use case

--- internal/domain/repository/repository.go
+++ internal/domain/repository/repository.go
@@ -8,4 +8,14 @@ type (
 		FindById(id string) (*model.Patient, error)
 		FindByIdentifier(identifier string) (*model.Patient, error)
 	}
+	MobileClinicRepository interface {
+		Save(model *model.Clinic) error
+		FindById(id string) (*model.Clinic, error)
+		FindByIdentifier(identifier string) (*model.Clinic, error)
+	}
+	AppointmentRepository interface {
+		Save(model *model.Appointment) error
+		FindById(id string) (*model.Appointment, error)
+		FindByIdentifier(identifier string) (*model.Appointment, error)
+	}
 )
