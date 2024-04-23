bf6d1931dcf6512ad9ea4230c31a64a8dc0a51d7
Brian <briannbig@gmail.com>
2024-04-23 19:07:14 +0300

    add gin logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/briannbig/afya-village/internal/application/middleware"
+	"github.com/briannbig/afya-village/internal/domain/queue"
+	"github.com/gin-gonic/gin"
+
+	"github.com/nats-io/nats.go"
+)
 
 func main() {
-	fmt.Println("Afya Village")
+	log.Printf("<<<<<<<<<<<<<<<Afya Village>>>>>>>>>>>>>>>")
+
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Printf("could not connect to queue --- %s", err.Error())
+		os.Exit(1)
+	}
+
+	producers := queue.RegisterProducers(nc)
+
+	log.Printf("initialized producers: size %v", len(producers))
+
+	r := router()
+
+	r.Run(":5050")
+
+}
+
+func router() *gin.Engine {
+
+	server := gin.Default()
+
+	setupLogger()
+
+	server.Use(gin.Recovery(), middleware.Logger())
+
+	server.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "Welcome to Afya Village"})
+	})
+
+	return server
+}
+
+func setupLogger() {
+	f, _ := os.Create("app-server.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 }
